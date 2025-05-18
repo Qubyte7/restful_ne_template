4d3a8c9dd4ee16e2eb8f67a8caf8ab38dbe84df1
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-05-18 09:11:36 +0200

    creating react query provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,25 +5,35 @@ import { PageNotFound } from "@/pages/Page-not-found.tsx";
 import ParkingSessionPage from "@/components/AdminPages/parkingSession/ParkingSessionPage.tsx";
 import VehicleManagementPage from "./components/AdminPages/vehicleManagement/VehicleManagementPage";
 import DashboardLayout from "./components/Layouts/DashboardLayout";
-import {AddNewSessionPage} from "./components/AdminPages/parkingSession/AddNewSessionPage";
+import { AddNewSessionPage } from "./components/AdminPages/parkingSession/AddNewSessionPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<DashboardLayout />}>
-            <Route path="parking-sessions" element={<ParkingSessionPage />} />
-            <Route path="parking-sessions/add-session" element={<AddNewSessionPage />} />
-            <Route path="vehicle-management" element={<VehicleManagementPage />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<DashboardLayout />}>
+              <Route path="parking-sessions" element={<ParkingSessionPage />} />
+              <Route path="parking-sessions/add-session" element={<AddNewSessionPage />} />
+              <Route path="vehicle-management" element={<VehicleManagementPage />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   )
 }
